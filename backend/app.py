1e26a675fe2f56057d4a1e046aa6ccf24dba9ead
reddyrohith49471 <145396409+reddyrohith49471@users.noreply.github.com>
2025-11-17 19:56:27 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -38,4 +38,4 @@ def create_app():
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5001, debug=True, use_reloader=False)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5001)))
