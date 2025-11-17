5d4e22455aa785347ef33b1172a6fb0f80e96fac
reddyrohith49471 <145396409+reddyrohith49471@users.noreply.github.com>
2025-11-17 19:43:44 +0530

    Update exception.py

--- backend/src/exception/exception.py
+++ backend/src/exception/exception.py
@@ -1,18 +1,17 @@
 import sys
-from app_logging.logger import logger
+from src.logging.logging import logging
 
-
-def error_message_detail(error, error_detail: sys):
-    _, _, exc_tb = error_detail.exc_info()
+def error_message_detail(error,error_detail:sys):
+    _,_,exc_tb = error_detail.exc_info()
     file_name = exc_tb.tb_frame.f_code.co_filename
-    return f"Error in [{file_name}] at line [{exc_tb.tb_lineno}]: {str(error)}"
-
+    error_message = "Error occured at this file [{0}] line number [{1}] error message[{2}]".format(file_name,exc_tb.tb_lineno,str(error))
+    return error_message
 
 class CustomException(Exception):
-    def __init__(self, error, error_detail: sys):
-        self.error_message = error_message_detail(error, error_detail)
-        logger.error(self.error_message)
-        super().__init__(self.error_message)
+    def __init__(self,error_message,error_detail:sys):
+        super().__init__(error_message)
+        self.error_message = error_message_detail(error_message,error_detail=error_detail)
 
     def __str__(self):
         return self.error_message
+
